54a8b35a3504eebbde2c1e1a621d5b5b99f0e164
igor <igorantonow314@gmail.com>
2023-05-18 17:56:20 +0300

    Add recognition from audiofiles

--- SpeechaBot/main.py
+++ SpeechaBot/main.py
@@ -25,12 +25,18 @@ def start_processing(message):
             "(don't forget to give me permission to read messages)\n")
     bot.reply_to(message, text)
 
-@bot.message_handler(content_types=['voice'])
+@bot.message_handler(content_types=['voice', 'audio'])
 def audio_processing(message):
-    file_info = bot.get_file(message.voice.file_id)
+    if message.voice:
+        file_id = message.voice.file_id
+        file_type = 'oog'
+    else:
+        file_id = message.audio.file_id
+        file_type = 'mp3' # TODO: verification!
+    file_info = bot.get_file(file_id)
     file_name = file_info.file_path.split('/')[1]
     file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(config.token, file_info.file_path))
-    audio = audio_converter(file_name, file)
+    audio = audio_converter(file_name, file, file_type=file_type)
     response = text_recognition(audio)
     empty_folders()
     bot.reply_to(message, response)
@@ -45,14 +51,17 @@ def videomessage_processing(message):
     empty_folders()
     bot.reply_to(message, response)
 
-def audio_converter(file_name, file):
+def audio_converter(file_name, file, file_type='oog'):
     if not os.path.exists(audio_folder):
         os.makedirs(audio_folder)
-    path_ogg = os.path.join(audio_folder, f"{file_name}.ogg")
+    path_source = os.path.join(audio_folder, f"{file_name}.{file_type}")
     path_wav = os.path.join(audio_folder, f"{file_name}.wav")
-    with open(path_ogg, 'wb') as f:
+    with open(path_source, 'wb') as f:
         f.write(file.content)
-    sound = AudioSegment.from_ogg(path_ogg)
+    if file_type == 'oog':
+        sound = AudioSegment.from_ogg(path_source)
+    elif file_type == 'mp3':
+        sound = AudioSegment.from_mp3(path_source)
     sound.export(path_wav, format="wav")
     return path_wav
 
